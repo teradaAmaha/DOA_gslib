486bf430c819f9c099a5cadf1f8e4f4bb873249f
Okada <g020g1039@g.neec.ac.jp>
2021-05-16 13:44:47 +0900

    asset追加しました。Player.hとEnemy.h用意しました。（中身空です）

--- DOA/DOA/main.cpp
+++ DOA/DOA/main.cpp
@@ -10,15 +10,6 @@ class MyGame : public gslib::Game {
     World world_;
     // 開始
     void start() override {
-        // テクスチャの読み込み
-        gsLoadTexture(TexturePlayer, "Assets/SHIP.png");
-        gsLoadTexture(TextureEnemy, "Assets/ENEMY.png");
-        // プレーヤーを追加
-        world_.add_actor(new Player{ &world_, GSvector2{ 0.0f, 240.0f } });
-        // 敵を3体追加
-        world_.add_actor(new Enemy{ &world_, GSvector2{ 640.0f, 120.0f } });
-        world_.add_actor(new Enemy{ &world_, GSvector2{ 640.0f, 240.0f } });
-        world_.add_actor(new Enemy{ &world_, GSvector2{ 640.0f, 360.0f } });
     }
     // 更新
     void update(float delta_time) override {
