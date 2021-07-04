c0bc92f475163e17424312e8227f56b705d27313
Okada <g020g1039@g.neec.ac.jp>
2021-07-04 17:19:12 +0900

    フェッチ用

--- DOA/Shooting3D_June_02/GamePlayScene.cpp
+++ DOA/Shooting3D_June_02/GamePlayScene.cpp
@@ -8,7 +8,7 @@
 #include "Camera.h"
 #include "Field.h"
 #include "World.h"
-
+#include "Enemy.h"
 
 World world_;
 void GamePlayScene::start()
@@ -17,6 +17,12 @@ void GamePlayScene::start()
     gsLoadMesh(Mesh_Asteroid01, "Assets/Model/prop_asteroid_01.msh");
     // 背景用画像の読み込み
     gsLoadTexture(Texture_BgTileNebulaGreen, "Assets/BG/tile_nebula_green_dff.png");
+    //プレーヤー弾画像読み込み
+    gsLoadTexture(Texture_EffectLazerOrange, "Assets/Effect/fx_lazer_orange_dff.png");
+    // 敵弾画像を読み込み
+    gsLoadTexture(Texture_EffectLazerCyan, "Assets/Effect/fx_lazer_cyan_dff.png");
+    // 敵メッシュの読み込み
+    gsLoadMesh(Mesh_Enemy, "Assets/Model/vehicle_enemyShip.msh");
 
     // フィールドの追加
     world_.add_field(new Field{ Texture_BgTileNebulaGreen });
@@ -25,7 +31,7 @@ void GamePlayScene::start()
     // ライトの追加
     world_.add_light(new Light{ &world_ });
     // プレーヤの追加
-    world_.add_actor(new Player{ &world_, GSvector3{ 0.0f, 0.0f, 0.0f } });
+    world_.add_actor(new Player{ &world_, GSvector3{ 0.0f, -60.0f, 0.0f } });
     // 隕石を３個追加
     world_.add_actor(new Asteroid{ &world_,
          GSvector3{ 0.0f , 100.0f, 0.0f }, GSvector3{ 0.0f , -1.0f, 0.0f } });
@@ -33,6 +39,9 @@ void GamePlayScene::start()
          GSvector3{  30.0f , 100.0f, 0.0f }, GSvector3{ 0.0f , -1.0f, 0.0f } });
     world_.add_actor(new Asteroid{ &world_,
          GSvector3{ -30.0f , 100.0f, 0.0f }, GSvector3{ 0.0f , -1.0f, 0.0f } });
+    // 敵を追加
+    world_.add_actor(new Enemy{ &world_, GSvector3{ -20.0f , 100.0f, 0.0f } });
+
 }
 
 void GamePlayScene::update(float delta_time)
